67eb91490f8875c30030cd10bde32bd689e0bdae
Tunisia Artope <artope@yahoo.com>
2022-12-23 02:29:35 +0000

    add remove method for Binary Search Tree

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Udemy
+
 // a class that creates a node
 class Node{
   constructor(value){
@@ -34,7 +36,7 @@ class BinarySearchTree{
     let currentNode = this.root;
     //loop through as long as tree is true (or 'while(currentNode !== null)')
     while(true){
-      //if the input value equals the current nodes value, return undefined
+      //if the input value equals the current nodes value, return undefined (if the value already exists)
       if(value === currentNode.value) return undefined;
       //if the input value is less than the current nodes value {if the current nodes left is empty, assign the new node to it, and return the tree}
       if(value < currentNode.value){
@@ -81,7 +83,97 @@ class BinarySearchTree{
   }
 
   //removes the node with the given value
-  remove(value){}
+  remove(value){
+    //if the tree is empty, return false
+    if(this.root === null) return false;
+
+    //start at root with variable currentNode
+    let currentNode = this.root;
+    //make a parent node to currentNode, set to null because the root is the top node
+    let parentNode = null;
+
+    //loop through as long as currentNode is not null
+    while(currentNode !== null){
+      //if input value is less than current nodes value {traverse parentNode to currentNode, and currentNode to left}
+      if(value < currentNode.value){
+        parentNode = currentNode;
+        currentNode = currentNode.left;
+      //else if input value is greater than current nodes value {traverse parentNode to currentNode, and currentNode to right}
+      }else if(value > currentNode.value){
+        parentNode = currentNode;
+        currentNode = currentNode.right;
+      }else if(value === currentNode.value){
+        //There's a match:
+        //option 1: if currentNode has no right child
+        if(currentNode.right === null){
+          //if root is chosen for removal {assign currentNode.left to root}
+          if(parentNode === null){
+            this.root = currentNode.left;
+          }else{
+            //if parentNode > current nodes value {assign currentNodes left value to parentNodes left pointer}
+            if(parentNode.value > currentNode.value){
+              parentNode.left = currentNode.left;
+            //else if parentNode < current nodes value, {assign currentNodes left value to parentNodes right pointer}
+            }else if(parentNode.value < currentNode.value){
+              parentNode.right = currentNode.left;
+            }
+          }
+        //option 2: currentNodes right child doesn't have a left child
+        //else if currentNodes right then left value is null {assign currentNode's left value to it}
+        }else if(currentNode.right.left === null){
+          currentNode.right.left = currentNode.left;
+          //if root is chosen for removal {assign currentNodes right value to root}
+          if(parentNode === null){
+            this.root = currentNode.right;
+          //else, parentNode > current nodes value {assign currentNodes right value to parentNodes left pointer}
+          }else{
+            if(parentNode.value > currentNode.value){
+              parentNode.left = currentNode.right;
+            //or if parentNode < current nodes value, assign currentNodes right value to parentNodes right pointer
+            }else if(parentNode.value < currentNode.value){
+              parentNode.right = currentNode.right;
+            }
+          }
+        //option 3: currentNodes right child, has a left child
+        }else{
+          //find the right child's left most child
+          //assign leftMost to currentNodes right, left value
+          let leftMost = currentNode.right.left;
+          //assign leftMostParent to currentNodes right value
+          let leftMostParent = currentNode.right;
+          //as long as the leftMost's left value is not null
+          while(leftMost.left !== null){
+            //traverse leftMostParent to leftMost and leftMost to leftMost's left value
+            leftMostParent = leftMost;
+            leftMost = leftMost.left;
+          }
+          //assign leftMost's right values to the leftMostParents left pointer (LMP gets the subtree of LM's right)
+          leftMostParent.left = leftMost.right;
+          //assign leftMost left pointer to currentNodes left value (LM takes the place of currentNode)
+          leftMost.left = currentNode.left;
+          //assign leftMost right pointer to currentNodes right value (LM takes the place of currentNode)
+          leftMost.right = currentNode.right;
+          //if root is chosen for removal {assing leftMost to root}
+          if(parentNode === null){
+            this.root = leftMost;
+          //otherwise, if currentNode value is less than parent node value {assign leftMost to parentNode left}
+          }else{
+            if(currentNode.value < parentNode.value){
+              parentNode.left = leftMost;
+            //otherwise, if currentNode value is greater than parent node value {assign leftMost to parentNode right}
+            }else if(currentNode.value > parentNode.value){
+              parentNode.right = leftMost;
+            }
+          }
+        }
+        //move out of option 3 else statement, set currentNodes left/right to null, and return true if removed
+        currentNode.left = null;
+        currentNode.right = null;
+        return currentNode;
+      }
+    }
+  }
+    
 
   printListArray(){
     const array = [];
@@ -113,8 +205,11 @@ tree.insert(11);
 tree.insert(2);
 tree.insert(16);
 tree.insert(7);
+tree.insert(6);
+tree.insert(15);
 // tree.insert(10);  //should return undefined because value is already in tree
-console.log(tree.find(10));
+// console.log(tree.find(10));
+console.log(tree.remove(10));
 
 console.log(tree);
 console.log(tree.printListArray());
